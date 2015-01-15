15e827bef5868090245346b4673ce2f6c1c9b874
jonathanln <jonathanln@hotmail.com>
2015-01-15 02:52:03 -0500

    Ajout librarie, modif Card, modif Hand
    - Ajout de la librairie compilée PokerEval qui retourne une valeur pour
    la meilleure main dans un ensemble de 7 cartes.
    - Ajout d'une méthode pour générer le code associé à une carte (ex : Ace
    Club = Ac). Le code est utilisé par la librairie PokerEval

--- src/MonkeyPoker/MonkeyPoker/Poker Game Classes/Hand.cs
+++ src/MonkeyPoker/MonkeyPoker/Poker Game Classes/Hand.cs
@@ -9,10 +9,12 @@ namespace MonkeyPoker.Poker_Game_Classes
     class Hand
     {
         public Card[] cards;
+        public uint value;
 
-        public Hand(Card card1, Card card2, Card card3, Card card4, Card card5)
+        public Hand(Card card1, Card card2, Card card3, Card card4, Card card5, uint handValue)
         {
             cards = new Card[5] {card1, card2, card3, card4, card5};
+            value = handValue;
         }
     }
 }
