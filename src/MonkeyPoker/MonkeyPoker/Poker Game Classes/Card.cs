8c8bcb8d06e2c7479c084a18b91cb8029f610a4b
jonathanln <jonathanln@hotmail.com>
2015-01-15 01:39:31 -0500

    Ajout de classes de bases pour le jeu de poker
    Get a random card object, etc.

---
+++ src/MonkeyPoker/MonkeyPoker/Poker Game Classes/Card.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyPoker.Poker_Game_Classes
+{
+    public enum CardSuit
+    {
+        Clubs,
+        Hearts,
+        Spades,
+        Diamonds
+    }
+
+    public enum CardRank
+    {
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Height,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
+    }
+
+    class Card
+    {
+        public CardSuit suit;
+        public CardRank rank;
+
+        public Card(CardSuit cardSuit, CardRank cardRank)
+        {
+            suit = cardSuit;
+            rank = cardRank;
+        }
+    }
+}
---
+++ src/MonkeyPoker/MonkeyPoker/Poker Game Classes/Hand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyPoker.Poker_Game_Classes
+{
+    class Hand
+    {
+        public Card[] cards;
+
+        public Hand(Card card1, Card card2, Card card3, Card card4, Card card5)
+        {
+            cards = new Card[5] {card1, card2, card3, card4, card5};
+        }
+    }
+}
